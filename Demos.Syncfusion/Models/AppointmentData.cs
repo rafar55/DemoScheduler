b5cd7010a11dd735e196276c6c3a682174f55ea2
Rafael Romero <rafaromero4407@gmail.com>
2020-11-04 15:35:31 -0600

    Add project files.

---
+++ Demos.Syncfusion/Models/AppointmentData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demos.Sf.Models
+{
+    public class AppointmentData
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
---
+++ Demos.Syncfusion/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Demos.Sf.Models;
+using Syncfusion.Blazor.Schedule;
+
+namespace Demos.Sf.Pages
+{
+    public partial class Index
+    {
+        private List<AppointmentData> _appointmentData = new List<AppointmentData>
+        {
+            new AppointmentData
+            {
+                Id = 1,
+                Subject = "Ir al Banco",
+                StartTime = DateTime.Parse("2020-11-4 15:30"),
+                EndTime = DateTime.Parse("2020-11-4 16:30:00")
+            },
+            new AppointmentData
+            {
+                Id = 1,
+                Subject = "Ir con lula a comer",
+                StartTime = DateTime.Parse("2020-11-5 16:40:00"),
+                EndTime = DateTime.Parse("2020-11-5 19:30:00")
+            }
+        };
+
+    }
+}
