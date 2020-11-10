b4fc362027ed2626e604571341107a212a21e614
Rafael Romero <rafaromero4407@gmail.com>
2020-11-10 09:20:32 -0600

    View Change

--- Demos.Syncfusion/Pages/Index.razor.cs
+++ Demos.Syncfusion/Pages/Index.razor.cs
@@ -14,9 +14,13 @@ namespace Demos.Sf.Pages
     public partial class Index
     {
 
+        private List<Appointment> _allAppointments;
+
+
         public Index()
         {
             AppointmentsData = new List<Appointment>();
+            CurrentView = View.Month;
         }
 
 
@@ -25,11 +29,14 @@ namespace Demos.Sf.Pages
 
         public DateTime SelectedDate { get; set; }
 
+        public View CurrentView { get; set; }
+
         protected List<Appointment> AppointmentsData { get; set; } 
 
 
         protected async override Task OnInitializedAsync()
         {
+            CurrentView = View.Month;
             SelectedDate = DateTime.Today;
             await LoadData();
             await base.OnInitializedAsync();
@@ -38,14 +45,28 @@ namespace Demos.Sf.Pages
 
         public async Task LoadData()
         {
-            AppointmentsData = await HttpClient.GetFromJsonAsync<List<Appointment>>("sample-data/appointments.json");          
+            if(_allAppointments == null || !_allAppointments.Any()) _allAppointments = await HttpClient.GetFromJsonAsync<List<Appointment>>("sample-data/appointments.json");
+
+
+            var startDate = new DateTime(SelectedDate.Year, SelectedDate.Month, 1).AddDays(-7);
+            var endDate = new DateTime(SelectedDate.Year, SelectedDate.Month, DateTime.DaysInMonth(SelectedDate.Year, SelectedDate.Month)).AddDays(7);
+
+            AppointmentsData = _allAppointments.Where(x => x.StartTime >= startDate && x.EndTime <= endDate).ToList();
         }
 
 
 
-        public void CalendarValueChanged(ChangedEventArgs<DateTime> args)
+        public async  Task CalendarValueChanged(ChangedEventArgs<DateTime> args)
         {
-            SelectedDate = args.Value;            
+            SelectedDate = args.Value;
+            await LoadData();
+        }
+
+        public async Task OnCalendarViewChange(ChangeEventArgs e)
+        {
+            var value = (string) e.Value;
+            CurrentView = (View) Enum.Parse(typeof(View), value);
+            await LoadData();
         }
 
     }
